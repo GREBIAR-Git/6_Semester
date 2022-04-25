193cb49b8d0460c4fafc9bfc79b57d4e9d25be1d
mikhail <mikhail.allyanov@gmail.com>
2022-04-26 02:46:18 +0300

    8 finished

--- Системное программирование/Лабораторные/8(системное)/1.c
+++ Системное программирование/Лабораторные/8(системное)/1.c
@@ -3,6 +3,32 @@
 #include <string.h>
 #include "WinMain.c"
 
+void HyperCopy(char ** here, char ** fromHere)
+{
+	free(*here);
+	*here = (char *)calloc('\n', strlen(*fromHere) * sizeof(char));
+	memcpy(*here, *fromHere, strlen(*fromHere) * sizeof(char));
+	return;
+}
+
+HBITMAP CopyBitmap(HBITMAP hbmOn) {
+    HDC hdcSrc = CreateCompatibleDC(NULL);
+    HDC hdcDst = CreateCompatibleDC(NULL);
+    HBITMAP hbmNew;
+    BITMAP bm, bm1;
+
+    GetObject(hbmOn, sizeof(bm), &bm);
+    SelectObject(hdcSrc, hbmOn);
+    hbmNew = CreateBitmap( bm.bmWidth, bm.bmHeight, bm.bmPlanes,bm.bmBitsPixel,NULL);
+    SelectObject(hdcDst, hbmNew);
+    BitBlt(hdcDst, 0, 0, bm.bmWidth, bm.bmHeight, hdcSrc, 0, 0, SRCCOPY);
+
+    DeleteDC(hdcSrc);
+    DeleteDC(hdcDst);
+
+    return hbmNew;
+}
+
 VOID UpdateWin(HWND hwnd)
 {
 	RECT window;  
@@ -17,45 +43,69 @@ VOID UpdateWin1(HWND hwnd,RECT window)
 	InvalidateRect(hwnd, &window, 1);
 }
 
-struct Buffer
-{
-	char text[20];
-	HBITMAP picture;
-};
+HBITMAP pic;
+char * text;
 
 LRESULT CALLBACK FrameWndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
 	switch (Message) {
-	case WM_CREATE: 
+	case WM_CREATE:
 	{
 		ShowWindow(hwnd, SW_SHOWMAXIMIZED);
 		break;
 	}
 	case WM_LBUTTONDOWN:
 	{
-		char * text = "Test1";
-		HGLOBAL hgBuffer= GlobalAlloc(GMEM_DDESHARE, strlen(text)+1);
-		char * chBuffer= (char*)GlobalLock(hgBuffer);
-		strcpy(chBuffer, text);
 		if(OpenClipboard(NULL))
 		{
-			EmptyClipboard();
-			SetClipboardData(CF_TEXT, hgBuffer);
+			if(IsClipboardFormatAvailable(CF_BITMAP))
+			{
+				HBITMAP picTmp = (HBITMAP) GetClipboardData(CF_BITMAP); // надо скопировать именно а то теряется потом
+				pic = CopyBitmap(picTmp);
+				
+				text = NULL;
+			} 
+			else if (IsClipboardFormatAvailable(CF_TEXT))
+			{
+				HGLOBAL hgBuffer = (HGLOBAL) GetClipboardData(CF_TEXT);
+				if(hgBuffer != NULL)
+				{  
+					char * chBuffer = (char *) GlobalLock(hgBuffer);
+					HyperCopy(&text, &chBuffer);
+					GlobalUnlock(hgBuffer);
+					pic = NULL;
+				}
+			}
 			CloseClipboard();
-			GlobalUnlock(hgBuffer);
 		}
 		UpdateWin(hwnd);
 		break;
 	}
-	case WM_RBUTTONUP:
+	case WM_RBUTTONDOWN:
 	{
-		HBITMAP pic = (HBITMAP)LoadImage(NULL, "0.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
-		if(OpenClipboard(NULL))
-		{
-			EmptyClipboard();
-			SetClipboardData(CF_BITMAP, pic);
-			CloseClipboard();
-			UpdateWin(hwnd);
+		if(pic != NULL)
+		{  
+			if(OpenClipboard(NULL))
+			{
+				EmptyClipboard();
+				printf("\nD\t%d", pic);
+				SetClipboardData(CF_BITMAP, pic);
+				CloseClipboard();
+			}
 		}
+		else if(text != NULL)
+		{  
+			HGLOBAL hgBuffer= GlobalAlloc(GMEM_DDESHARE, strlen(text)+1);
+			char * chBuffer= (char*)GlobalLock(hgBuffer);
+			strcpy(chBuffer, text);
+			if(OpenClipboard(NULL))
+			{
+				EmptyClipboard();
+				SetClipboardData(CF_TEXT, hgBuffer);
+				CloseClipboard();
+				GlobalUnlock(hgBuffer);
+			}
+		}
+		UpdateWin(hwnd);
 		break;
 	}
 	case WM_PAINT:
@@ -67,98 +117,14 @@ LRESULT CALLBACK FrameWndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lPa
 		RECT window;
 		GetClientRect(hwnd,&window);
 		FillRect(hdc, &window, (HBRUSH)(RGB(255,255,255)));
-		if(OpenClipboard(NULL))
-		{
-			if(IsClipboardFormatAvailable(CF_BITMAP))
-			{
-				HBITMAP pic = (HBITMAP) GetClipboardData(CF_BITMAP);
-				if(pic != NULL)
-				{  
-					SelectObject(memDc, pic);
-					BitBlt(hdc, 0,50,window.right,window.bottom,memDc,0,0,SRCCOPY);
-				} 
-			} 
-			else if (IsClipboardFormatAvailable(CF_TEXT))
-			{
-				HGLOBAL hgBuffer = (HGLOBAL) GetClipboardData(CF_TEXT);
-				if(hgBuffer != NULL)
-				{  
-					char * chBuffer = (char *) GlobalLock(hgBuffer);
-					TextOut(hdc, 0, 0,  chBuffer, strlen(chBuffer));
-					GlobalUnlock(hgBuffer);
-				}
-			}
-			else if (IsClipboardFormatAvailable(3))
-			{
-				printf("3");
-			}
-			else if (IsClipboardFormatAvailable(4))
-			{
-				printf("4");
-			}
-			else if (IsClipboardFormatAvailable(5))
-			{
-				printf("5");				
-			}
-			else if (IsClipboardFormatAvailable(6))
-			{
-				printf("6");
-			}
-			else if (IsClipboardFormatAvailable(7))
-			{
-				printf("7");
-			}
-			else if (IsClipboardFormatAvailable(8))
-			{
-				printf("8");
-			}
-			else if (IsClipboardFormatAvailable(9))
-			{
-				printf("9");
-			}
-			else if (IsClipboardFormatAvailable(10))
-			{
-				printf("10");
-			}
-			else if (IsClipboardFormatAvailable(11))
-			{
-				printf("11");
-			}
-			else if (IsClipboardFormatAvailable(12))
-			{
-				printf("12");
-			}
-			else if (IsClipboardFormatAvailable(13))
-			{
-				printf("13");
-			}
-			else if (IsClipboardFormatAvailable(14))
-			{
-				printf("14");
-			}
-			else if (IsClipboardFormatAvailable(15))
-			{
-				//DragQueryFiles();
-				printf("15");
-			}
-			else if (IsClipboardFormatAvailable(16))
-			{
-				printf("16");
-			}
-			else if (IsClipboardFormatAvailable(17))
-			{
-				printf("17");
-			}
-			else if (IsClipboardFormatAvailable(18))
-			{
-				printf("18");
-			}
-			else
-			{
-				printf("19");
-			}
-			
-			CloseClipboard();
+		if(pic != NULL)
+		{  
+			SelectObject(memDc, pic);
+			BitBlt(hdc, 0,50,window.right,window.bottom,memDc,0,0,SRCCOPY);
+		} 
+		else if(text != NULL)
+		{  
+			TextOut(hdc, 0, 0,  text, strlen(text));
 		}
 		DeleteDC(memDc);
 		EndPaint(hwnd, &ps);
@@ -174,10 +140,10 @@ LRESULT CALLBACK FrameWndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lPa
 		{
 			case VK_LEFT:
 			{
-				if(OpenClipboard(NULL))//открываем буфер обмена
+				if(OpenClipboard(NULL))
 				{
-					EmptyClipboard(); //очищаем буфер
-					CloseClipboard(); //закрываем буфер обмена
+					EmptyClipboard();
+					CloseClipboard();
 					UpdateWin(hwnd);
 				}
 				break;
