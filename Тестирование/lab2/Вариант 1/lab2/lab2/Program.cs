ec5cc507d3a573f061fb5c2f9bc78947ec779bf7
mikhail <mikhail.allyanov@gmail.com>
2022-05-03 01:27:51 +0300

    testing lab 2

---
+++ Тестирование/lab2/Вариант 1/lab2/lab2/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using System;
+
+namespace lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Input: ");
+                string expression = Console.ReadLine();
+                if (string.IsNullOrEmpty(expression))
+                {
+                    Console.Write("Empty expression");
+                    continue;
+                }
+                try
+                {
+                    var result = Parse(expression);
+                    Console.WriteLine(result.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.Write(e.ToString()); // e. первая штучка говорит какая именна ошибка
+                }
+            }
+        }
+
+
+        static double Parse(string expression)
+        {
+            return CSharpScript.EvaluateAsync<double>(expression).Result;
+        }
+    }
+}
---
+++ Тестирование/lab2/Вариант 1/readme.txt
@@ -0,0 +1,1 @@
+для запуска нужен nuget: codeanalysis.csharp.scripting
