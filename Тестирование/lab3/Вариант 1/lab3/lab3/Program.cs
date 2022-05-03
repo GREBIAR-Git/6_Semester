d5d4f0f788a9b3b69cbec566f3ea2d6576548dfd
Mikhail <mikhail.allyanov@gmail.com>
2022-05-03 09:08:13 +0300

    testing lab 3

---
+++ Тестирование/lab3/Вариант 1/lab3/lab3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("a*x^2+b*x+c=0");
+                double a = ReadLine("a");
+                double b = ReadLine("b");
+                double c = ReadLine("c");
+
+                double D = Math.Pow(b, 2) - 4 * a * c;
+
+                if (D < 0)
+                {
+                    double x_r = (- b) / (2 * a);
+                    double x_i = (Math.Sqrt(-D)) / (2 * a);
+                    Console.WriteLine("x1 = " + x_r + " + (" + x_i + ")i");
+                    Console.WriteLine("x2 = " + x_r + " - (" + x_i + ")i");
+                }
+                else
+                {
+                    double x1 = (Math.Sqrt(D) - b) / (2 * a);
+                    double x2 = (-Math.Sqrt(D) - b) / (2 * a);
+                    Console.WriteLine("x1 = " + x1);
+                    Console.WriteLine("x2 = " + x2);
+                }
+                Console.WriteLine("\n\n");
+            }
+        }
+
+        static double ReadLine(string name)
+        {
+            double value;
+            while (true)
+            {
+                try
+                {
+                    Console.Write(name + " = ");
+                    string val = Console.ReadLine();
+                    value = double.Parse(val);
+                    break;
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+            return value;
+        }
+    }
+}
