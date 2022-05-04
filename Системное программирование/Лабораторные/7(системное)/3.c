1fb534ed436192a7d2f9bb37003a95df0f3545ab
mikhail <mikhail.allyanov@gmail.com>
2022-05-05 00:40:31 +0300

    system programming lab 7 SHORT

--- Системное программирование/Лабораторные/7(системное)/3.c
+++ Системное программирование/Лабораторные/7(системное)/3.c
@@ -36,31 +36,10 @@ int put(HWND hwnd)
 {
     HANDLE hBmpMappingFile;
     LPVOID hBmpMapFileAddr;
-
     hBmpFile = CreateFileA("0.bmp", GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-    if ((int)hBmpFile == (int)INVALID_HANDLE_VALUE)
-    {
-        printf("\tCreateFileA error %d\n", GetLastError());
-    }
-
     RECT rect;
     GetClientRect(hwnd,&rect);
-
    	hBmpMappingFile = CreateFileMappingA(hBmpFile, NULL, PAGE_READONLY, 0, 0, TEXT("kat"));
-    if (hBmpMappingFile == NULL)
-    {
-        printf("\tCreateFileMappingA error %d\n", GetLastError());
-    }
-    else if ((int)hBmpMappingFile == (int)ERROR_ALREADY_EXISTS)
-    {
-        printf("\tCreateFileMappingA already exists\n");
-    }
-
    	hBmpMapFileAddr = MapViewOfFile(hBmpMappingFile, FILE_MAP_READ, 0, 0, 0);
-    if (hBmpMapFileAddr == NULL)
-    {
-        printf("\tMapViewOfFile error %d\n", GetLastError());
-    }
-
     return (INT_PTR)TRUE;
 }
--- Системное программирование/Лабораторные/7(системное)/4.c
+++ Системное программирование/Лабораторные/7(системное)/4.c
@@ -38,36 +38,21 @@ int get(HWND hwnd)
 {
     HANDLE hBmpMappingFile;
     LPVOID hBmpMapFileAddr;
-
     RECT rect;
     GetClientRect(hwnd,&rect);
-
    	hBmpMappingFile = OpenFileMappingA(FILE_MAP_READ, FALSE, TEXT("kat"));
-    if (hBmpMappingFile == NULL)
-    {
-        printf("\tCreateFileMappingA error %d\n", GetLastError());
-    }
-
    	hBmpMapFileAddr = MapViewOfFile(hBmpMappingFile, FILE_MAP_READ, 0, 0, 0);
-    if (hBmpMapFileAddr == NULL)
-    {
-        printf("\tMapViewOfFile error %d\n", GetLastError());
-    }
-
 	hBmpFile = (HBITMAP)hBmpMapFileAddr;
-	
+
     BITMAPFILEHEADER *bFileHeader = (BITMAPFILEHEADER*)hBmpMapFileAddr;
     BITMAPINFO *bInfo = (BITMAPINFO*)((char*)hBmpMapFileAddr+14);
     HDC hdc = GetDC(hwnd);
     hBmpFile = CreateDIBitmap(hdc,&(bInfo->bmiHeader), CBM_INIT, (char*)hBmpMapFileAddr+bFileHeader->bfOffBits, bInfo, DIB_PAL_COLORS);
     HDC hMemDC = CreateCompatibleDC(hdc);
-
     SelectObject(hMemDC,hBmpFile);
     StretchBlt(hdc, 0, 0, rect.right, rect.bottom, hMemDC, 0, 0, bInfo->bmiHeader.biWidth, bInfo->bmiHeader.biHeight, SRCCOPY); 
-    
     ReleaseDC(hwnd,hdc);
     DeleteDC(hMemDC);
     DeleteObject(hBmpFile);
-
     return (INT_PTR)TRUE;
 }
